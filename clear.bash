fc7b13313bb8e9f076aa8f3c2bf0e3635165f30a
yunocare <fabian.schulze@st.ovgu.de>
2018-10-04 18:14:09 +0300

    Minor changes

--- clear.bash
+++ clear.bash
@@ -1,12 +1,26 @@
 #!/bin/bash
-echo "Clearing .cache..."
+echo "Clearing cache..."
 rm -rf ~/.cache/
 mkdir ~/.cache/
 echo "Successfully finished."
 
 # clear terminal history
+echo "Clearing terminal history..."
 rm ~/.bash_history
 touch ~/.bash_history
+echo "Successfully finished."
 
 # empty recycle bin
+echo "Emptying recycle bin..."
 rm -rf ~/.local/share/Trash/*
+echo "Successfully finished."
+
+# remove VIM history
+echo "Removing VIM history..."
+rm ~/.viminfo
+echo "Successfully removed"
+
+# remove .thumbnails  directory
+echo "Removing .thumbnails directory..."
+rm -rf ~/.thumbnails/
+echo "Successfully finshed."
