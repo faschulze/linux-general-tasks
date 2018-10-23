7948974f8c9ba1bfbfe8e196db8d7ee710922a9c
yunocare <fabian.schulze@st.ovgu.de>
2018-10-23 22:27:45 +0300

    Added Install files

--- install/install_openjdk-8.bash
+++ install/install_openjdk-8.bash
@@ -7,7 +7,8 @@ sudo apt-get update
 sudo apt-get install openjdk-8-jdk
 sudo apt install openjdk-8-jdk-headless 
 
-# sudo apt-get install openjdk-8-source #this is optional, the jdk source code
+# sudo apt-get install openjdk-8-source 
+#this is optional, the jdk source code
 apt-cache search jdk
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk
 export PATH=$PATH:/usr/lib/jvm/java-8-openjdk/bin
